09f37ed773dbe2427f5985c66c70eef8178cb032
Ahmad <ahmad.shubita@harri.com>
2024-04-18 16:53:46 +0200

    Add view All items screen for now and popular movies

--- app/src/main/java/com/ahmadshubita/moviesapp/ui/core/navigation/MainNavHost.kt
+++ app/src/main/java/com/ahmadshubita/moviesapp/ui/core/navigation/MainNavHost.kt
@@ -7,25 +7,27 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.ahmadshubita.moviesapp.ui.bottombar.MoviesDestination
+import com.ahmadshubita.moviesapp.ui.bottombar.allItemsGraph
 import com.ahmadshubita.moviesapp.ui.bottombar.moviesGraph
 import com.ahmadshubita.moviesapp.ui.bottombar.peopleGraph
 import com.ahmadshubita.moviesapp.ui.bottombar.tvGraph
 
 @Composable
 fun MainNavHost(
-    navController: NavHostController = rememberNavController(),
-    startDestination: String = MoviesDestination.route,
-    modifier: Modifier = Modifier,
-    isBottomNavVisible: MutableState<Boolean>
+        navController: NavHostController = rememberNavController(),
+        startDestination: String = MoviesDestination.route,
+        modifier: Modifier = Modifier,
+        isBottomNavVisible: MutableState<Boolean>,
 ) {
     NavHost(
-        navController = navController,
-        startDestination = startDestination,
-        modifier = modifier
+            navController = navController,
+            startDestination = startDestination,
+            modifier = modifier
     ) {
         // Define the Graphs here and you can define nested graphs also.
         moviesGraph(navController = navController, isBottomNavVisible = isBottomNavVisible, navigateToDetails = {})
         tvGraph(navController = navController, isBottomNavVisible = isBottomNavVisible, navigateToDetails = {})
         peopleGraph(navController = navController, isBottomNavVisible = isBottomNavVisible, navigateToDetails = {})
+        allItemsGraph(navController = navController, isBottomNavVisible = isBottomNavVisible, navigateToDetails = {})
     }
 }
